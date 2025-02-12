b9394860d281669ce3d938a7756eddb928163dd1
chanielm <chanieljm@gmail.com>
2025-02-12 17:13:48 -0500

    Console Menu Created

--- HW-2C/Program.cs
+++ HW-2C/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int choice;
+
+do {
+    Console.WriteLine("\n**************");
+    Console.WriteLine("1 - New Game");
+    Console.WriteLine("2 - Load Game");
+    Console.WriteLine("3 - Options");
+    Console.WriteLine("4 - Quit");
+    Console.WriteLine("**************\n");
+
+    if (!int.TryParse(Console.ReadLine(), out choice)) choice = -1;
+    Console.WriteLine($"You selected {choice}.");
+
+    switch (choice) {
+        case 1:
+        Console.WriteLine("Starting New Game...");
+        break;
+
+        case 2:
+        Console.WriteLine("Loading Current File...");
+        break;
+
+        case 3:
+        Console.WriteLine("Options Menu");
+        break;
+
+        case 4:
+        Console.WriteLine("Terminating Program...");
+        break;
+
+        default:
+        Console.WriteLine("Invalid Entry. Try re-entering?");
+        break;
+    }
+    
+} while (choice != 4);
